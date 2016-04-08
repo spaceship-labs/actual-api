32df408af5ca84346ddb3bc49c83dd990b935481
Luis <luis19prz@gmail.com>
2016-04-08 18:24:30 -0500

    mods sql

--- mods.sql
+++ mods.sql
@@ -1,2 +1,4 @@
 ALTER TABLE  `Product` CHANGE  `GuaranteeText`  `GuaranteeText` LONGTEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
 ALTER TABLE  `Product` CHANGE  `PackageContent`  `PackageContent` LONGTEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE  `Product` ADD  `Color` VARCHAR( 255 ) NOT NULL
+ALTER TABLE  `productfilter` ADD  `IsMultiple` TINYINT( 1 ) NOT NULL
