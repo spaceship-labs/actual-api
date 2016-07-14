c6cd982a6c5d3ad896b8d875a5ab23945fcb7a0d
tugorez <tugorez@undefined>
2016-07-14 02:10:14 -0500

    added policie for permissions

--- api/models/Permission.js
+++ api/models/Permission.js
@@ -9,6 +9,7 @@ module.exports = {
   attributes: {
     name: {
       type: 'string',
+      unique: true,
       required: true
     },
     controller: {
---
+++ api/policies/isAllowed.js
@@ -0,0 +1,20 @@
+module.exports = function (req, res, next) {
+  var user       = req.user.id;
+  var controller = req.options.controller;
+  var action     = req.options.action;
+  Permission.find({
+    action: action,
+    controller: controller
+  })
+    .populate('owners')
+    .exec(function(err, permissions) {
+      var allowed = (permissions || []).find(function(permission){
+        return (permission.owners || []).indexOf(user) !== -1;
+      });
+      if (!allowed) {
+        return res.unauthorized('user is not authorized');
+      } else {
+        next();
+      }
+    });
+};
