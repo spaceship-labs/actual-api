8e95cbc1245a6246da23f9752ca6379e1c23bda7
Luis <luis19prz@gmail.com>
2016-07-20 12:55:57 -0500

    cronjob with primises

--- api/services/CronJobs.js
+++ api/services/CronJobs.js
@@ -1,13 +1,15 @@
 var cron = require('cron').CronJob;
 var async = require('async');
+var Promise = require('bluebird')
 
 module.exports.init = function(){
+  console.log('initing cronJobs');
   var cronJobs = [
     {
       fn: function(d){
         getPromos();
       }
-      , time:'0 */2 * * * *'
+      , time:'0 0 */1 * * *'
       //s,m,h,d del mes,m,d de la semana
     }
   ].forEach(function(v){
@@ -27,13 +29,8 @@ function getPromos(){
     .populate('CustomBrands')
     .then(function(promos){
       if(promos){
-        async.forEachSeries(promos ,updatePromo, function(err){
-          if(err){
-            console.log(err);
-          }else{
-            sails.log.info('Termino update de todas las promos');
-          }
-
+        Promise.each(promos,updatePromo).then(function(){
+          sails.log.info('Termino update de todas las promos');
         });
       }else{
         console.log('No habia promos por actualizar');
@@ -43,7 +40,7 @@ function getPromos(){
 }
 
 
-function updatePromo(promo, callback){
+function updatePromo(promo){
   var categories = promo.Categories.map(function(c){return c.id});
   var filtervalues = promo.FilterValues.map(function(fv){return fv.id});
   var groups = promo.Groups.map(function(g){return g.id});
@@ -62,28 +59,24 @@ function updatePromo(promo, callback){
     excluded: excluded
   };
   sails.log.info('Searching:');
-  Search.promotionCronJobSearch(opts).then(function(res){
+  return Search.promotionCronJobSearch(opts).then(function(res){
     if(res && _.isArray(res)){
       var products = res.map(function(p){return p.id});
       sails.log.info('Actualizando con ' + products.length + ' products' );
-      Promotion.update({id: promo.id}, {Products: products})
+      return Promotion.update({id: promo.id}, {Products: products})
         .then(function(updated){
-          callback();
           return null;
         })
         .catch(function(err){
           console.log(err);
-          callback();
           return null;
         });
     }else{
-      callback();
       return null;
     }
     //return res;
   }).catch(function(err){
     console.log(err);
-    callback();
     //return err;
   });
 }
