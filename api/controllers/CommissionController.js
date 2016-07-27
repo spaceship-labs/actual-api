bca056502ae4d47d12e6a319756c46ef136e1023
tugorez <tugorez@undefined>
2016-07-27 17:52:54 -0500

    reimplemented commission

--- api/controllers/CommissionController.js
+++ api/controllers/CommissionController.js
@@ -1,3 +1,18 @@
 module.exports = {
+  create: function(req, res) {
+    var form       = req.allParams();
+    var commission = form.commission;
+    Commission.create(commission).exec(function(err, commission){
+      if (err) {return res.negotiate(err);}
+      return res.json(commission);
+    });
+  },
 
+  find: function(req, res) {
+
+  },
+
+  search: function(req, res) {
+
+  }
 }
