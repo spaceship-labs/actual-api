21dadabd090221d1ad4a0d2e50c41ffa770b7700
Luis <luis19prz@gmail.com>
2016-08-04 10:25:54 -0500

    update client sap

--- api/services/SapService.js
+++ api/services/SapService.js
@@ -1,21 +1,28 @@
 var baseUrl = 'http://sapnueve.homedns.org:8080/';
 var request = require('request');
 var Promise = require('bluebird');
+var appendQuery = require('append-query');
+
 module.exports = {
   updateClient: updateClient
 };
 
-function updateClient(cardcode){
+function updateClient(cardcode, form){
   return new Promise(function(resolve, reject){
-    request.get(baseUrl + 'Contact', function(err, response, body){
+    var url = baseUrl + 'Contact(' + cardcode + ')';
+    var endPoint = appendQuery(url, form);
+    console.log('endPoint');
+    console.log(endPoint);
+    request.post( endPoint, function(err, response, body){
       if(err){
         reject(err);
       }else{
-        sails.log.info(response);
-        sails.log.info(body);
         resolve(response);
       }
     });
   });
+}
+
+function createClient(){
 
 }
