561072465ac6a297fcec535067cfe1d81e1cb55c
tugorez <tugorez@undefined>
2016-08-07 22:43:06 -0500

    reimplemented commissions service

--- api/services/Commissions.js
+++ api/services/Commissions.js
@@ -1,9 +1,77 @@
 var _ = require('underscore');
 
 module.exports = {
-  user: userCommissions,
+  userRates: userRates
 };
 
+function userRates(idUser, dateFrom, dateTo) {
+  return User.findOne(idUser).populate('role')
+    .then(function(user){
+      var type = user.role.map(function(role){return role.id;});
+      return [
+        Commission.find({type: type}),
+        userSales(idUser, dateFrom, dateTo),
+        companySales(user.companyMain, dateFrom ,dateTo)
+      ];
+    })
+    .spread(function(comissions, totalUser, totalStore) {
+      return comissions.reduce(function(acum, current) {
+        if (totalUser >= current.individualGoal  && totalStore >= current.storeGoal) {
+          return {
+            individualRate: acum.individualRate + current.individualRate,
+            storeRate: acum.storeRate + current.storeRate,
+          };
+        }
+        return acum;
+      }, {individualRate: 0, storeRate: 0});
+    });
+}
+
+function userSales(idUser, dateFrom, dateTo) {
+  var query = queryDate({User: idUser}, dateFrom, dateTo);
+  return Payment.find(query).then(sumTotalPayments);
+}
+
+function companySales(idCompany, dateFrom, dateTo) {
+  var query = queryDate({Store: idCompany}, dateFrom, dateTo);
+  return Payment.find(query).then(sumTotalPayments);
+}
+
+function sumTotalPayments(payments) {
+  return currencyNormalize(payments).reduce(function(acum, current) {
+    return acum + current.total;
+  }, 0);
+}
+
+function currencyNormalize(payments) {
+  return payments.map(function(payment) {
+    if (payment.currency != 'mxn') {
+      payment.total = payment.ammount * payment.exchangeRate;
+    } else {
+      payment.total = payment.ammount;
+    }
+    return payment;
+  });
+}
+
+function queryDate(query, dateFrom, dateTo) {
+  var dateFrom = new Date(dateFrom);
+  var dateTo   = new Date(dateTo);
+  return _.assign(query, {
+    createdAt: {
+      '>=': dateFrom,
+      '<': addOneDay(dateTo)
+    }
+  });
+}
+
+function addOneDay(date) {
+  var date = new Date(date);
+  date.setDate(date.getDate() + 1);
+  return date;
+}
+
+/*
 function userCommissions(idUser, dateFrom, dateTo) {
   return User.findOne(idUser).populate('role')
     .then(function(user){
@@ -60,3 +128,4 @@ function addOneDay(date) {
   date.setDate(date.getDate() + 1);
   return date;
 }
+*/
