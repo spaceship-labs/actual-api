a17ce0a28df90a87d65f05f64a11b5184a736522
tugorez <tugorez@undefined>
2016-08-22 14:19:55 -0500

    added goal resource

---
+++ api/models/Goal.js
@@ -0,0 +1,28 @@
+/**
+ * Goal.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    goal: {
+      type: 'float',
+      required: true
+    },
+    sellers: {
+      type: 'integer',
+      required: true
+    },
+    date: {
+      type: 'date',
+      required: true
+    },
+    company: {
+      model: 'company',
+      required: true
+    }
+  }
+};
+
