886422549ade6fecf2d8b272bcf3e4923fd3aff2
tugorez <tugorez@undefined>
2016-08-29 00:09:17 -0500

    basis of commissions

---
+++ api/models/Commission.js
@@ -0,0 +1,17 @@
+/**
+ * Commission.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    payment: {
+      model: 'payment',
+      required: true,
+      unique: true
+    }
+  }
+};
+
