cec80fda46503ea2f02e378b3583d00ec3709c96
Luis <luis19prz@gmail.com>
2016-11-10 10:48:38 -0500

    sync service products icons

--- api/controllers/ImportController.js
+++ api/controllers/ImportController.js
@@ -3,10 +3,6 @@ var fs = require('fs');
 var async = require('async');
 var pathService = require('path');
 var Promise = require('bluebird');
-var productsList = [];
-var photosUploaded = 0;
-var waitingTime = 0;
-var prodCount = 0;
 
 module.exports = {
   importImagesSap: function(req, res){
@@ -19,17 +15,21 @@ module.exports = {
     prodCount = 0;
     waitingTime = 0;
     sails.log.debug('limit : ' + limit);
-    Product.find({},{select:['ItemCode','PicturName','icon_filename']}).sort('Available DESC').skip(skip).limit(limit).exec(function(err, prods){
-      if(err){
+    Product.find({},{select:['ItemCode','PicturName','icon_filename']})
+      .sort('Available DESC')
+      .skip(skip)
+      .limit(limit)
+      .then(function(products){
+        var uploader = new SyncService.ProductImageUploader();
+        return uploader.updateIcons(products);
+      })
+      .then(function(productsList){
+        res.json(productsList);        
+      })
+      .catch(function(err){
         console.log(err);
-        //throw(err);
-      }
-      updateIcons(prods, function(result){
-        console.log(result);
-        res.json(productsList);
+        res.negotiate(err);
       });
-
-    });
   },
   importBrokersToUsers: function(req, res){
     SyncService.importBrokersToUsers()
@@ -42,138 +42,6 @@ module.exports = {
         res.negotiate(err);
       });
   }
-}
-
-
-function updateIcon(prod, callback){
-  var itemCode = prod.ItemCode;
-  var icon = prod.icon_filename;
-  var imgName = prod.PicturName;
-  prodCount++;
-
-  //var excludes = ['CO32166','CO10438','CO31751','CO32938','CO43242','CO44544','CO44540','CO44849','CO46687','CO46688','CO47735','CO03412','CO24328','ST01203','ST01312','ST02342','ST07239','ST09227','ST09473'];
-  var excludes = [];
-  //sails.log.warn('Articulo: ' + itemCode +' | Icono : ' + icon);
-
-  if(typeof imgName!= 'undefined' && imgName && imgName != '' && icon == null && excludes.indexOf(itemCode) < 0 ){
-    var rootDir = '/home/luis/Pictures/ImagenesSAP/';
-    var path = rootDir  + imgName;
-    //path = path.replace(/ /g, '\\');
-    var rsfile = fs.createReadStream(path);
-    var internalFile = streamToFile( rsfile, imgName );
-    sails.log.debug('Articulo a subir : ' + itemCode  + ' , Prod index: ' + prodCount + ' imagen:' + imgName + ' | icon: ' + icon);
-    sails.log.info('Fotos subidas: ' + photosUploaded + ' | Tiempo de espera: ' + waitingTime);
-
-    if(photosUploaded%5 == 0 && photosUploaded != 0){
-      waitingTime = 60000;
-    }else{
-      waitingTime = 0;
-    }
-
-    setTimeout(function(){
-
-      Product.updateAvatarSap(internalFile,{
-        dir : 'products',
-        profile: 'avatar',
-        id : itemCode,
-      },function(e,product){
-        if(e){
-          console.log(e);
-          callback();
-        }else{
-          sails.log.info('Articulo Subido: ' + itemCode);
-          productsList.push({ItemCode: itemCode, status: 'con imagen SAP'});
-          photosUploaded++;
-          callback();
-        }
-      });
-
-    }, waitingTime);
-  }
-  else if( excludes.indexOf(itemCode) > -1 ){
-    //sails.log.warn('Producto excluido ' + itemCode);
-    //sails.log.warn(imgName);
-    productsList.push({ItemCode: itemCode, status: 'verificar imagen'});
-    callback();
-  }
-
-  else if(icon && icon!='' && imgName && imgName != ''){
-    //sails.log.warn('callback else');
-    productsList.push({ItemCode: itemCode, status: 'con imagen SAP'});
-    callback();
-
-  }
-  else if(icon && icon!='' && !imgName){
-    //sails.log.warn('callback else');
-    productsList.push({ItemCode: itemCode, status: 'con imagen'});
-    callback();
-
-  }
-  else{
-    //sails.log.warn('callback else');
-    //sails.log.warn('Sin imagen');
-    productsList.push({ItemCode: itemCode, status: 'faltante'});
-    callback();
-  }
-}
-
-
-function updateIcons(prods,callback){
-  async.forEachSeries(prods ,updateIcon, function(err){
-    if(err){
-      console.log(err);
-      callback({done:false});
-    }
-    callback({done:true});
-  });
-}
-
-function streamToFile(inStream, filename) {
-
-  var Upstream = require('skipper/standalone/Upstream');
-  var stream = require('stream');
-  var imgName = filename;
-
-  /**
-   * A transform stream that counts the bytes flowing through it
-   *
-   * The byte count can be read from this.byteCount property on the class' instance.
-   */
-  function ByteCountStream(opts){
-    stream.Transform.call(this, opts);
-    this.byteCount = 0;
-    var extension = Common.getImgExtension(imgName);
-    this.filename = 'generico.' + extension;
-    this.type = 'image/' + extension;
-    this._transform = function(chunk, encoding, done){
-      this.byteCount += chunk.length
-      return done(null, chunk)
-    }
-  }
-  ByteCountStream.prototype = Object.create(stream.Transform.prototype);
-  //ByteCountStream.prototype.constructor = ByteCountStream;
-
-
-  // An Upstream is basically what we get when we call `req.file()` on an incoming request
-  var file = new Upstream()
-  // Because Skipper uses the stream's fd attribute to save the location information on the remote
-  // filesystem's storage, we must pipe the stream through an intermediary. Why? Because an fs
-  // read stream uses that same fd attribute to store it's file descriptor, which, when changed
-  // during data transfer, will silently stop the flow of bytes.
-  // As a nice side-effect, we can use this intermediary to count the bytes being uploaded, so we
-  // have this information available just like when we would upload a file through http upload.
-      , intermediary = new ByteCountStream()
-
-  inStream.pipe(intermediary)
-
-  // file.upload() only prepares the uploader to start receiving files - the .writeFile() actually
-  // sends a file to the receiver. And, we must also tell the upstream that there are no more
-  // files to be received, otherwise it will just keep waiting for more files to come
-  file.writeFile(intermediary)
-  file.noMoreFiles()
-
-  // The `req.file()` is now fully simulated - continue as if http upload occured
-  return file
-}
+};
 
 
--- api/services/SyncService.js
+++ api/services/SyncService.js
@@ -3,10 +3,12 @@ var request = require('request');
 var Promise = require('bluebird');
 var BROKER_ROLE_ID = "57915b6d81e8947014bec270";
 var ACTUAL_HOME_ID = '57bf590089c75aed0825c3f2';
+var fs = require('fs');
 
 module.exports = {
   syncProducts: syncProducts,
-  importBrokersToUsers: importBrokersToUsers
+  importBrokersToUsers: importBrokersToUsers,
+  ProductImageUploader: ProductImageUploader
 };
 
 function syncProducts(){
@@ -51,15 +53,6 @@ function importBrokersToUsers(){
     newUsers = _.uniq(newUsers, function(u){
       return u.email;
     });
-    /*
-    sails.log.info('brokerUsersEmails: ' + brokerUsersEmails.length);
-    sails.log.info('newUsersEmail : ' + newUsersEmails.length);
-    sails.log.info(brokerUsersEmails);
-    sails.log.info(newUsersEmails);
-    var diff = _.difference(newUsersEmails, brokerUsersEmails);
-    sails.log.info('diff');
-    sails.log.info(diff);
-    */
     return User.create(newUsers);
   })
   .then(function(created){
@@ -91,3 +84,166 @@ function generateRandomString(length) {
     }
     return retVal;
 }
+
+
+function streamToFile(inStream, filename) {
+
+  var Upstream = require('skipper/standalone/Upstream');
+  var stream = require('stream');
+  var imgName = filename;
+
+  function ByteCountStream(opts){
+    stream.Transform.call(this, opts);
+    this.byteCount = 0;
+    var extension = Common.getImgExtension(imgName);
+    this.filename = 'generico.' + extension;
+    this.type = 'image/' + extension;
+    this._transform = function(chunk, encoding, done){
+      this.byteCount += chunk.length;
+      return done(null, chunk);
+    };
+  }
+  ByteCountStream.prototype = Object.create(stream.Transform.prototype);
+  var file = new Upstream(), intermediary = new ByteCountStream();
+  inStream.pipe(intermediary);
+  file.writeFile(intermediary);
+  file.noMoreFiles();
+  return file;
+}
+
+var photosUploaded = 0;
+var productsList = [];
+var waitingTime = 0;
+
+function updateIconFromBase64(product){
+  var base64Img = product.base64Img;
+  var itemCode = product.ItemCode;
+  return new Promise(function(resolve, reject){
+
+    if(photosUploaded%5 === 0 && photosUploaded !== 0){
+      waitingTime = 60000;
+    }else{
+      waitingTime = 0;
+    }    
+
+    setTimeout(function(){
+      Product.updateAvatarSap(internalFile,{
+        dir : 'products',
+        profile: 'avatar',
+        id : itemCode,
+      },function(e,product){
+        if(e){
+          console.log(e);
+          resolve();
+        }else{
+          photosUploaded++;
+          productsList.push({ItemCode: itemCode, status: 'con imagen SAP'});
+          resolve();
+        }
+      });
+
+    }, waitingTime);    
+  });
+}
+
+function updateIcon(product){
+  return new Promise(function(resolve, reject){
+    photosUploaded = photosUploaded || 0;
+    productsList = productsList || [];
+    waitingTime = waitingTime || 0;
+    var itemCode = product.ItemCode;
+    var icon = product.icon_filename;
+    var imgName = product.PicturName;
+    var excludes = [];
+    //sails.log.warn('Articulo: ' + itemCode +' | Icono : ' + icon);
+
+    if(hasImage(imgName) && icon === null && excludes.indexOf(itemCode) < 0 ){
+      var internalFile = getInternalFile(imgName);
+      sails.log.debug('Articulo a subir : ' + itemCode  + ' , product index: ' + prodCount + ' imagen:' + imgName + ' | icon: ' + icon);
+      sails.log.info('Fotos subidas: ' + photosUploaded + ' | Tiempo de espera: ' + waitingTime);
+
+      if(photosUploaded%5 === 0 && photosUploaded !== 0){
+        waitingTime = 60000;
+      }else{
+        waitingTime = 0;
+      }
+
+      setTimeout(function(){
+        Product.updateAvatarSap(internalFile,{
+          dir : 'products',
+          profile: 'avatar',
+          id : itemCode,
+        },function(e,product){
+          if(e){
+            console.log(e);
+            resolve();
+          }else{
+            photosUploaded++;
+            productsList.push({ItemCode: itemCode, status: 'con imagen SAP'});
+            resolve();
+          }
+        });
+
+      }, waitingTime);
+    }
+    else if( excludes.indexOf(itemCode) > -1 ){
+      productsList.push({ItemCode: itemCode, status: 'verificar imagen'});
+      resolve();
+    }
+
+    else if(icon && icon!=='' && imgName && imgName !== ''){
+      productsList.push({ItemCode: itemCode, status: 'con imagen SAP'});
+      resolve();
+
+    }
+    else if(icon && icon!=='' && !imgName){
+      productsList.push({ItemCode: itemCode, status: 'con imagen'});
+      resolve();
+    }    
+    else{
+      resolve();
+    }
+  });
+}
+
+function getInternalFile(imgName){
+  var rootDir = '/home/luis/Pictures/ImagenesSAP/';
+  var path = rootDir  + imgName;
+  var rsfile = fs.createReadStream(path);
+  var internalFile = streamToFile( rsfile, imgName );
+  return internalFile;
+}
+
+
+function hasImage(imgName){
+  return typeof imgName!= 'undefined' && imgName && imgName !== '';
+}
+
+function updateProductsIcons(products){
+  return Promise.each(products, updateIcon)
+    .then(function(){
+      return productsList;
+    });
+}
+
+function updateProductsIconsFromBase64(products){
+  return Promise.each(products, updateIcon)
+    .then(function(){
+      return productsList;
+    });
+}
+
+function ProductImageUploader(){
+  var photosUploaded = 0;
+  var productsList = [];  
+  var waitingTime = 0;
+  return {
+    photosUploaded: photosUploaded,
+    productsList: productsList,
+    waitingTime: waitingTime,
+    updateProductsIcons: updateProductsIcons,
+    updateProductsIconsFromBase64: updateIconFromBase64
+  };
+}
+
+
