7a0addf5472cd53c1cb0247046f691b04fcbc80f
Luis <luis19prz@gmail.com>
2017-02-04 10:57:12 -0500

    files debug

--- api/services/Files.js
+++ api/services/Files.js
@@ -27,7 +27,7 @@ function saveFiles(req,opts,cb){
 
   return new Promise(function(resolve, reject){
 
-    sails.log.info('saving files');
+    sails.log.info('saving files', $files);
 
     if(req._fileparser.upstreams.length && $files.length > 0){
 
