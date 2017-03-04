a47924632e4813c4a1c64ef3b9169dbfcd531f0c
Luis <luis19prz@gmail.com>
2017-03-04 08:46:11 -0500

    aux email

--- api/services/Email.js
+++ api/services/Email.js
@@ -213,6 +213,10 @@ function sendOrder(client, user, order, products, payments, ewallet, store) {
   var to               = new helper.Email(clientEmail, client.CardName);
   var subject          = 'Confirmación de compra | Folio #' + order.folio;
   var content          = new helper.Content("text/html", emailBody);
+
+  var toAux = new helper.Email('luisperez@spaceshiplabs.com', 'Luis Perez');
+  personalization.addTo(toAux);
+
   personalization.addTo(to);
   personalization.setSubject(subject);
   /*
@@ -345,14 +349,17 @@ function sendQuotation(client, user, quotation, products, payments, transfers, s
   var to               = new helper.Email(clientEmail, client.CardName);
   var subject          = 'Cotización | Folio #' + quotation.folio;
   var content          = new helper.Content("text/html", emailBody);
-  /**/
+  /*
     var to2 = new helper.Email('oreinhart@actualg.com', 'Oliver Reinhart');
     var to3 = new helper.Email('tugorez@gmail.com', 'Juanjo Tugorez');
     var to4 = new helper.Email('luis19prz@gmail.com', 'Luis Perez');
     if(user.email !== 'oreinhart@actualg.com') personalization.addTo(to2);
     if(user.email !== 'tugorez@gmail.com') personalization.addTo(to3);
     if(user.email !== 'luis19prz@gmail.com') personalization.addTo(to4);
-  /**/
+  */
+  var toAux = new helper.Email('luisperez@spaceshiplabs.com', 'Luis Perez');
+  personalization.addTo(toAux);
+
   personalization.addTo(to);
   personalization.setSubject(subject);
   mail.setFrom(from);
@@ -463,6 +470,10 @@ function sendFreesale(user, order, products, store) {
   var content          = new helper.Content("text/html", emailBody);
   personalization.addTo(to);
   personalization.setSubject(subject);
+
+  var toAux = new helper.Email('luisperez@spaceshiplabs.com', 'Luis Perez');
+  personalization.addTo(toAux);
+  
   /*
     var to2 = new helper.Email('oreinhart@actualg.com', 'Oliver Reinhart');
     var to3 = new helper.Email('tugorez@gmail.com', 'Juanjo Tugorez');
