2d672ca40920c5a0935dc3de6303ff735daac8f2
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-01-02 17:09:16 -0500

    BrokerController pagination fixed

--- api/controllers/BrokerController.js
+++ api/controllers/BrokerController.js
@@ -1,7 +1,7 @@
 module.exports = {
   async list(req, res) {
     try {
-      const { page = 0, limit = 0 } = req.allParams();
+      const { page = 1, limit = 10 } = req.allParams();
       const brokers = await BrokerSAP.find().paginate({ page, limit });
       res.ok(brokers);
     } catch (err) {
