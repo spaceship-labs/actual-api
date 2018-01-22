93c863d9640faa1c2d93791d2c390977898449c2
Luis <luis19prz@gmail.com>
2018-01-22 16:21:12 -0500

    changed qroo shipping days low season

--- api/controllers/QuotationController.js
+++ api/controllers/QuotationController.js
@@ -71,7 +71,7 @@ module.exports = {
 
   async findByIdQuickRead(req, res){
     try{
-      const {id} = req.params.all();
+      const {id} = req.allParams();
       const quotation = await Quotation.findOne({id: id});
       if(!quotation){
         return res.negotiate(new Error('Cotización no encontrada'));
