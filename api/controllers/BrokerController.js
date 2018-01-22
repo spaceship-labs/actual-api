2d524724bebdc6204ba6f091d073d845a81f1152
Luis <luis19prz@gmail.com>
2018-01-22 17:45:48 -0500

    broker pagination set to 1k

--- api/controllers/BrokerController.js
+++ api/controllers/BrokerController.js
@@ -1,7 +1,7 @@
 module.exports = {
   async list(req, res) {
     try {
-      const { page = 1, limit = 10 } = req.allParams();
+      const { page = 1, limit = 1000 } = req.allParams();
       const brokers = await BrokerSAP.find().paginate({ page, limit });
       res.ok(brokers);
     } catch (err) {
