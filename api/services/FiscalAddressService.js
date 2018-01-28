da758b4f831420036f7ea32acb5da43c728ef739
Luis <luis19prz@gmail.com>
2018-01-28 12:28:16 -0500

    moved update fiscal address logic to fiscal address service

---
+++ api/services/FiscalAddressService.js
@@ -0,0 +1,42 @@
+module.exports = {
+  updateFiscalAddress,
+  isValidSapFiscalClientUpdate
+};
+
+function isValidSapFiscalClientUpdate(sapData){
+	if(sapData.type === ClientService.CARDCODE_TYPE && ClientService.isValidCardCode(sapData.result)  ){
+		return true;
+	}	
+
+	if(sapData.type === ClientService.ERROR_TYPE){
+    throw new Error(sapData.result);
+  }
+  throw new Error('Error al actualizar datos fiscales en SAP');
+}
+
+async function updateFiscalAddress(params, req) {
+  var CardCode = req.user.CardCode;
+  var fiscalAddress = ClientService.mapFiscalFields(params);
+  delete params.AdresType;
+
+  try{
+    if(!params.LicTradNum || !ClientService.isValidRFC(params.LicTradNum)){
+      throw new Error('RFC no valido');
+    }
+    
+    const sapResult = await SapService.updateFiscalAddress(CardCode, fiscalAddress);
+    sails.log.info('updateFiscalAddress response', sapResult);
+    const sapData = JSON.parse(sapResult.value);
+
+    isValidSapFiscalClientUpdate(sapData);
+    const fiscalAddressesUpdated = await FiscalAddress.update({CardCode:CardCode}, fiscalAddress);
+    const fiscalAddressUpdated = fiscalAddressesUpdated[0];
+    const clientsUpdated =  await Client.update(
+      {CardCode: CardCode}, 
+      {LicTradNum: params.LicTradNum, cfdiUse: params.cfdiUse}
+    );
+    return fiscalAddressUpdated;
+  }catch(err){
+    throw new Error(err);
+  }
+}
