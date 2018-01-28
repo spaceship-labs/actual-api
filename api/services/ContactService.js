ee880d8d553da0bb4e87e909530d884bf0654485
Luis <luis19prz@gmail.com>
2018-01-28 12:23:10 -0500

    moved create contact logic to contactservice

---
+++ api/services/ContactService.js
@@ -0,0 +1,42 @@
+const _ = require('underscore');
+
+module.exports = {
+  createContact,
+  validateSapContactCreation
+};
+
+function validateSapContactCreation(sapData){
+  if(_.isArray(sapData)){
+    const contact = sapData[0];
+    if(contact.type === ClientService.PERSON_TYPE){
+      return true;
+    }
+    if(contact.type === ClientService.ERROR_TYPE){
+      throw new Error(contact.result);
+    }
+  }
+  throw new Error('Error al crear contacto en SAP');
+}
+
+
+async function createContact(params, req){
+  var cardCode = req.user.CardCode;
+  params.CardCode = cardCode;
+  params = ClientService.mapContactFields(params);
+
+  try{
+    const sapResult = await SapService.createContact(cardCode, params);
+    sails.log.info('response createContact', sapResult);
+    var sapData = JSON.parse(sapResult.value);
+    
+    validateSapContactCreation(sapData);
+    const CntctCode  = sapData[0].result;
+    params.CntctCode = parseInt(CntctCode);
+    const createdContact = await ClientContact.create(params);
+    return createdContact;
+  }
+  catch(err){
+    throw new Error(err);
+  }
+}
+
