3d087457c3efa8a505454d5957b584e460f8c267
Luis <luis19prz@gmail.com>
2018-02-12 13:23:58 -0500

    async /await tests

--- test/integration/controllers/AuthController.test.js
+++ test/integration/controllers/AuthController.test.js
@@ -1,35 +1,34 @@
 describe('Auth controller', function(){
 
 	describe('api status', function(){
-		it('should return a valid response',function(done){
+		it('should return a valid response', async function(){
 			var url = '/';
-			app.get(url)
-				.expect(200)
-				.then(function(res){
-					expect(res.body.status).to.contain("ok");
-					done();
-				})
-				.catch(function(err){
-					console.log('err', err);
-					done();
-				});
+			try{
+				const {body, status} = await app.get(url);
+				console.log('status', status);
+				console.log('body', body);
+				expect(body.status).to.contain("ok");
+				expect(status).to.equal(200);
+			}catch(e){
+				console.log('e', e);
+			}
 		});
 	});
 
 	describe('login process', function(){
-		it('should return a token after a valid login', function(done){
+		it('should return a token after a valid login', async function(){
 			var url = '/auth/signin';
-			app.post(url)
-				.send({
-					email: process.env.SAMPLE_ADMIN_USER_EMAIL,
-					password: process.env.SAMPLE_ADMIN_USER_PASSWORD,
-				})
-				.set('accept', 'json')
-				.then(function(res){
-					expect(res.body).to.have.property("token");
-					done();
-				});
-
+			try{
+				const {body} = await app.post(url)
+					.send({
+						email: process.env.SAMPLE_ADMIN_USER_EMAIL,
+						password: process.env.SAMPLE_ADMIN_USER_PASSWORD,
+					})
+					.set('accept', 'json');
+				expect(body).to.have.property("token");
+			}catch(e){
+				console.log('e', e);
+			}
 		});
 
 	});
