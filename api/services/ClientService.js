226e13e5be7bf9bfd73fa4a74623e3878f33b886
Luis <luis19prz@gmail.com>
2018-02-19 11:25:23 -0500

    only admit normal alphabet chars in the first 3 places of rfc

--- api/services/ClientService.js
+++ api/services/ClientService.js
@@ -10,7 +10,7 @@ const PERSON_TYPE = 'Person';
 const ERROR_TYPE = 'Error';
 const ACTUAL_EMAIL_DOMAIN = /@actualgroup.com$/;
 const DATE_REGEX = "((0[1-9]|[12]\\d|3[01])(01|03|05|07|08|10|12)|(0[1-9]|[12]\\d)02|(0[1-9]|[12]\\d|30)(04|06|09|11))(\\d{2})";
-const RFC_VALIDATION_REGEX = new RegExp("^(([A-Z,Ñ,&]{3,4})"+DATE_REGEX+"([A-Z|\\d]{3}))$");
+const RFC_VALIDATION_REGEX = new RegExp("^(([A-Z]{3,4})"+DATE_REGEX+"([A-Z|\\d]{3}))$");
 
 module.exports = {
 	ADDRESS_TYPE,
