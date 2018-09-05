bdbccdfcc3ca0aa0939920ace35ad0ea1794b664
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-09-05 12:14:18 -0500

    EwalletConfiguration model added and configured

---
+++ api/models/EwalletConfiguration.js
@@ -0,0 +1,20 @@
+/**
+ * EwalletConfiguration.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    exchangeRate: {
+      type: 'float',
+    },
+    expirationDate: {
+      type: 'datetime',
+    },
+    minimumPercentageToGeneratePoints: {
+      type: 'float',
+    },
+  },
+};
