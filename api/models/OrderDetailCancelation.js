ade964984ddfea820c630cf04a9b99ed6d7ecd5b
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-12-10 18:27:05 -0500

    OrderDetailCancelation added, update and add functions in cancelation service refectored

---
+++ api/models/OrderDetailCancelation.js
@@ -0,0 +1,28 @@
+/**
+ * OrderDetailCancelation.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    status: {
+      type: 'string',
+      enum: ['pending', 'authorized', 'rejected'],
+      defaultsTo: 'pending',
+    },
+    quantity: {
+      type: 'integer',
+    },
+    Order: {
+      model: 'Order',
+    },
+    Cancelation: {
+      model: 'OrderCancelation',
+    },
+    Detail: {
+      model: 'OrderDetail',
+    },
+  },
+};
