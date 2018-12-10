7b4b95291b9cfdfe2262210d97563b4a44382113
Luis <luis19prz@gmail.com>
2018-12-10 11:44:42 -0500

    changed shipping days qroo and yucatan

--- api/services/Shipping.js
+++ api/services/Shipping.js
@@ -117,11 +117,11 @@ async function buildShippingItem(
   let seasonDays;
 
   if (isMeridaWhsCode(stockItem.whsCode)) {
-    MAIN_SEASON_DAYS = 12; //ORIGINAL 11
-    LOW_SEASON_DAYS = 12; // ORIGINAL 10
+    MAIN_SEASON_DAYS = 18; //ORIGINAL 11
+    LOW_SEASON_DAYS = 18; // ORIGINAL 10
   } else {
-    MAIN_SEASON_DAYS = 12; //10 // ORIGINAL 6
-    LOW_SEASON_DAYS = 12; // ORIGINAL 5
+    MAIN_SEASON_DAYS = 10; //10 // ORIGINAL 6
+    LOW_SEASON_DAYS = 10; // ORIGINAL 5
   }
 
   if (season) {
