fd6081cdaa9b0914a4a536b10cc769b0500755f2
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-01-02 13:38:54 -0500

    test init

--- config/connections.js
+++ config/connections.js
@@ -31,75 +31,15 @@ module.exports.connections = {
     adapter: 'sails-disk',
   },
 
-  /***************************************************************************
-   *                                                                          *
-   * MySQL is the world's most popular relational database.                   *
-   * http://en.wikipedia.org/wiki/MySQL                                       *
-   *                                                                          *
-   * Run: npm install sails-mysql                                             *
-   *                                                                          *
-   ***************************************************************************/
   mongodb: {
     adapter: 'sails-mongo',
-    /*
-    host: process.env.MONGODB_HOST || 'YOUR_MONGODB_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    port: process.env.MONGODB_PORT || 27017,
-    user: process.env.MONGODB_USER || 'YOUR_MONGODB_USER',
-    password: process.env.MONGODB_PASS || 'YOUR_MONGODB_PASSWORD',
-    database: process.env.MONGODB_NAME || 'YOUR_MONGODB_DB',
-    */
     url: process.env.MONGODB_URL || 'YOUR_MONGODB_URL',
   },
 
-  /*
-  sqlserver: {
-    adapter: 'sails-sqlserver',
-    user: process.env.SAPDB_USER || '',
-    password: process.env.SAPDB_PASSWORD || '',
-    host: process.env.SAPDB_HOST || '',
-    database: process.env.SAPDB_NAME || '',
-    options: {
-      encrypt: true   // use this for Azure databases
-    }
-  },*/
-
-  /***************************************************************************
-   *                                                                          *
-   * MongoDB is the leading NoSQL database.                                   *
-   * http://en.wikipedia.org/wiki/MongoDB                                     *
-   *                                                                          *
-   * Run: npm install sails-mongo                                             *
-   *                                                                          *
-   ***************************************************************************/
-  someMongodbServer: {
+  testMongo: {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    database: process.env.LOCAL_MONGO,
   },
-
-  /***************************************************************************
-   *                                                                          *
-   * PostgreSQL is another officially supported relational database.          *
-   * http://en.wikipedia.org/wiki/PostgreSQL                                  *
-   *                                                                          *
-   * Run: npm install sails-postgresql                                        *
-   *                                                                          *
-   *                                                                          *
-   ***************************************************************************/
-  somePostgresqlServer: {
-    adapter: 'sails-postgresql',
-    host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_POSTGRES_USER',
-    password: 'YOUR_POSTGRES_PASSWORD',
-    database: 'YOUR_POSTGRES_DB',
-  },
-
-  /***************************************************************************
-   *                                                                          *
-   * More adapters: https://github.com/balderdashy/sails                      *
-   *                                                                          *
-   ***************************************************************************/
 };
--- test/integration/services/EwalletService.test.js
+++
@@ -1,8 +0,0 @@
-describe('EwalletService', () => {
-  describe('validateExpirationDate', () => {
-    it('should vertufy if expirationDate is equal to the current date and restart ewallets amounts', async () => {
-      const result = await EwalletService.validateExpirationDate();
-      console.log(result);
-    });
-  });
-});
