039955f49dae189ff55140c9af4e8ede8b94f9e8
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2019-01-03 17:14:41 -0500

    transfer ecommerce added

---
+++ test/integration/controllers/AlertController.test.js
@@ -0,0 +1,8 @@
+describe('AlertController', () => {
+  let token = null;
+  let user = null;
+  let alerts = null;
+  before(async () => {
+    const { email, password } = await CreateService.user();
+  });
+});
