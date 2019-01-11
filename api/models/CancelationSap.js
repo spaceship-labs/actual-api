a70835639c20d299f684997ad40dab6a011fb8af
EmmanuelYupit <emmanuelyupit08@gmail.com>
2019-01-11 09:35:44 -0500

    model unrelated

--- api/models/CancelationSap.js
+++ api/models/CancelationSap.js
@@ -6,9 +6,32 @@
  */
 
 module.exports = {
-
   attributes: {
-
-  }
+    type: {
+      type: 'string',
+    },
+    result: {
+      type: 'string',
+    },
+    Payments: {
+      collection: 'Payment',
+      via: 'cancelationsSapRef',
+    },
+    PaymentsCancel: {
+      collection: 'Payment',
+      via: 'canceledSap',
+    },
+    series: {
+      type: 'string',
+    },
+    CancelationsOrder: {
+      collection: 'OrderCancelation',
+      via: 'CancelationsSap',
+      columnName: 'RequestTransfer',
+    },
+    Products: {
+      collection: 'Product',
+      via: 'CancelationsSap',
+    },
+  },
 };
-
