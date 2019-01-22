f4ac025adc6ebc81c0e077546dcf99d07e53e3d0
Luis <luis19prz@gmail.com>
2019-01-22 09:39:08 -0500

    changed shipping days qroo

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -76,6 +76,6 @@ module.exports = {
   homeStatus(req, res) {
     const status =
       process.env.MODE !== 'production' ? 'ok sandbox!' : 'ok production!';
-    res.ok({ status, version: '1.2.12' });
+    res.ok({ status, version: '1.2.13' });
   },
 };
