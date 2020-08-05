2be189dfab20600c097060a9bd3e43f852538400
Sergio Can <gersiopumas@gmail.com>
2020-08-05 11:01:54 -0500

    Delivery dates

--- api/services/Shipping.js
+++ api/services/Shipping.js
@@ -117,18 +117,18 @@ async function buildShippingItem(
   let seasonDays;
 
   if (isMeridaWhsCode(stockItem.whsCode)) {
-    MAIN_SEASON_DAYS = 14; //ORIGINAL 13
-    LOW_SEASON_DAYS = 14; // ORIGINAL 13
+    MAIN_SEASON_DAYS = 12; //ORIGINAL 13
+    LOW_SEASON_DAYS = 12; // ORIGINAL 13
     if (stockItem.whsCode === CEDIS_MERIDA_WHS_CODE) {
-      MAIN_SEASON_DAYS = 12;
-      LOW_SEASON_DAYS = 12; // ORIGINAL 5
+      MAIN_SEASON_DAYS = 10;
+      LOW_SEASON_DAYS = 10; // ORIGINAL 5
     }
   } else {
-    MAIN_SEASON_DAYS = 8; //10 // ORIGINAL 5
-    LOW_SEASON_DAYS = 8; // ORIGINAL 5
+    MAIN_SEASON_DAYS = 9; //10 // ORIGINAL 5
+    LOW_SEASON_DAYS = 9; // ORIGINAL 5
     if (stockItem.whsCode === CEDIS_QROO_CODE) {
-      MAIN_SEASON_DAYS = 6;
-      LOW_SEASON_DAYS = 6; // ORIGINAL 5
+      MAIN_SEASON_DAYS = 8;
+      LOW_SEASON_DAYS = 8; // ORIGINAL 5
     }
   }
 
