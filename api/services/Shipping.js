ce5cae8c291f6dfb84df15c1fb58f07d19cc9694
Sergio Can <gersiopumas@gmail.com>
2020-11-09 15:31:12 -0500

    update delivery dates

--- api/services/Shipping.js
+++ api/services/Shipping.js
@@ -123,16 +123,16 @@ async function buildShippingItem(
     LOW_SEASON_DAYS = 6;
   } else {
     //in stores
-    MAIN_SEASON_DAYS = 8;
-    LOW_SEASON_DAYS = 8;
+    MAIN_SEASON_DAYS = 12;
+    LOW_SEASON_DAYS = 12;
     if (stockItem.whsCode === CEDIS_QROO_CODE && storeWarehouseId !== CEDIS_MERIDA_WHS_CODE) {
       // in cedis in cancun
-      MAIN_SEASON_DAYS = 6;
-      LOW_SEASON_DAYS = 6;
+      MAIN_SEASON_DAYS = 9;
+      LOW_SEASON_DAYS = 9;
     } else if (stockItem.whsCode === CEDIS_QROO_CODE && storeWarehouseId === CEDIS_MERIDA_WHS_CODE) {
       // in cedis 01 in merida
-      MAIN_SEASON_DAYS = 10;
-      LOW_SEASON_DAYS = 10;
+      MAIN_SEASON_DAYS = 11;
+      LOW_SEASON_DAYS = 11;
     }
   }
 
