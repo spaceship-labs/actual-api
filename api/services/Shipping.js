a047e5491481920fccfa04dd88b8d8c9aec110ad
Sergio Can <gersiopumas@gmail.com>
2021-05-14 11:08:23 -0500

    update shipping

--- api/services/Shipping.js
+++ api/services/Shipping.js
@@ -125,12 +125,12 @@ async function buildShippingItem(
     LOW_SEASON_DAYS = 6;
   } else {
     //in stores
-    MAIN_SEASON_DAYS = 7;
-    LOW_SEASON_DAYS = 7;
+    MAIN_SEASON_DAYS = 8;
+    LOW_SEASON_DAYS = 8;
     if (stockItem.whsCode === CEDIS_QROO_CODE && !isMeridaWhsCode(storeWarehouseId)) {
       // from cedis in cancun
-      MAIN_SEASON_DAYS = 6;
-      LOW_SEASON_DAYS = 6;
+      MAIN_SEASON_DAYS = 8;
+      LOW_SEASON_DAYS = 8;
     } else if (stockItem.whsCode === CEDIS_QROO_CODE && isMeridaWhsCode(storeWarehouseId)) {
       // from cedis 01 to any merida
       MAIN_SEASON_DAYS = 11;
