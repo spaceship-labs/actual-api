03075e28e5abc677117f44e8bbbaba2060dfbb9b
Sergio Can <gersiopumas@gmail.com>
2021-11-09 13:42:15 -0500

    update shipping

--- api/services/Shipping.js
+++ api/services/Shipping.js
@@ -120,28 +120,28 @@ async function buildShippingItem(
 
   if (isMeridaWhsCode(stockItem.whsCode)) {
     // in Merida stores
-    MAIN_SEASON_DAYS = 7;
-    LOW_SEASON_DAYS = 7;
+    MAIN_SEASON_DAYS = 9;
+    LOW_SEASON_DAYS = 9;
   } else {
     //in stores
-    MAIN_SEASON_DAYS = 7;
-    LOW_SEASON_DAYS = 7;
+    MAIN_SEASON_DAYS = 15;
+    LOW_SEASON_DAYS = 15;
     if (stockItem.whsCode === CEDIS_QROO_CODE && !isMeridaWhsCode(storeWarehouseId)) {
       // from cedis in cancun
-      MAIN_SEASON_DAYS = 7;
-      LOW_SEASON_DAYS = 7;
+      MAIN_SEASON_DAYS = 15;
+      LOW_SEASON_DAYS = 15;
     } else if (stockItem.whsCode === CEDIS_QROO_CODE && isMeridaWhsCode(storeWarehouseId)) {
       // from cedis 01 to any merida
-      MAIN_SEASON_DAYS = 10;
-      LOW_SEASON_DAYS = 10;
+      MAIN_SEASON_DAYS = 15;
+      LOW_SEASON_DAYS = 15;
     } else if (["02","03","05","82"].includes(stockItem.whsCode) && isMeridaWhsCode(storeWarehouseId)) {
       // from qroo store to any merida
-      MAIN_SEASON_DAYS = 12;
-      LOW_SEASON_DAYS = 12;
-    } else { 
+      MAIN_SEASON_DAYS = 15;
+      LOW_SEASON_DAYS = 15;
+    }  else { 
       // use minimum
-      MAIN_SEASON_DAYS = 7;
-      LOW_SEASON_DAYS = 7;
+      MAIN_SEASON_DAYS = 9;
+      LOW_SEASON_DAYS = 9;
     }
   }
 
