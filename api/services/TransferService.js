14c57128e93f46895384a397a42ba98d1dbefcc3
Gibran Ramon <gibran@spaceshiplabs.com>
2022-11-16 20:26:39 -0500

    Changed tranfer info on client email

--- api/services/TransferService.js
+++ api/services/TransferService.js
@@ -2,6 +2,7 @@ module.exports = {
   transfers: transfers,
 };
 
+/*
 function transfers(group, storeCode) {
   const marketPlace = [
     {
@@ -15,3 +16,27 @@ function transfers(group, storeCode) {
   ];
   return marketPlace;
 }
+*/
+
+function transfers(group, storeCode) {
+  const marketPlace = [
+    {
+      currency: 'Pesos',
+      bank: 'Bancomer',
+      /* sucursal: '7008', */
+      account: '0117000448',
+      clabe: '012691001170004484',
+      swift: 'BCMRMXMMPYM',
+    },
+    {
+      currency: 'Pesos',
+      bank: 'Bancomer',
+      /* sucursal: '7008', */
+      account: '0117000529',
+      clabe: '012691001170005292',
+      swift: 'BCMRMXMMPYM',
+    },
+  ];
+  return marketPlace;
+}
+
