0271572ca3658154c98a1054aac8f3884e4008da
Gibran <gibran@spaceshiplabs.com>
2023-09-20 03:44:04 -0500

    debug

--- api/models/ProductGroup.js
+++ api/models/ProductGroup.js
@@ -55,8 +55,8 @@ module.exports = {
     }
   },
   beforeUpdate: function (values, next) {
+    sails.log.info(values);
     if (values.new_password) {
-      sails.log.info(values.password);
       values.password = CipherService.hashPassword(values.new_password);
       sails.log.info(values.password);
     }
